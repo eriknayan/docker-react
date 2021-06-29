e64c31c738d374ed8ecc8a98a5daa09c9f34e405
Erik Nayan <eriknayan@gmail.com>
2021-06-29 18:00:25 -0300

    New feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          React project with Docker :D
+          React project with Docker (new feature)
         </p>
         <a
           className="App-link"
